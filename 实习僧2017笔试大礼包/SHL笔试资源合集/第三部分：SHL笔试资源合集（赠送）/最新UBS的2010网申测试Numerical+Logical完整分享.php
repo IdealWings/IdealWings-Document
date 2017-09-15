e25edc6d908fdb65b67ee997fc0698bb82afce31
734645298@qq.com <734645298@qq.com>
2017-09-15 18:46:08 +0800

    fix

---
+++ 实习僧2017笔试大礼包/SHL笔试资源合集/第三部分：SHL笔试资源合集（赠送）/最新UBS的2010网申测试Numerical+Logical完整分享.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gbk" />
+<title>  应届生求职招聘论坛 应届生BBS</title>
+<meta name="keywords" content="应届生BBS 应届生论坛 求职招聘论坛 " />
+<meta name="description" content=" 应届生求职招聘论坛 中国大学生求职第一论坛，是广大应届生毕业生求职、交流、互动的平台。 - Discuz! Board" />
+<meta name="generator" content="Discuz! 7.2" />
+<meta name="author" content="Discuz! Team and Comsenz UI Team" />
+<meta name="copyright" content="2001-2009 Comsenz Inc." />
+<meta name="MSSmartTagsPreventParsing" content="True" />
+<meta http-equiv="MSThemeCompatible" content="Yes" />
+<meta http-equiv="x-ua-compatible" content="ie=7" />
+<link rel="archives" title="应届生求职招聘论坛" href="http://bbs.yingjiesheng.com/archiver/" />
+<link rel="stylesheet" type="text/css" href="forumdata/cache/style_31_common.css?MxH" />
+<script type="text/javascript">var STYLEID = '31', IMGDIR = 'images/default', VERHASH = 'MxH', charset = 'gbk', discuz_uid = 896987, cookiedomain = 'yingjiesheng.com', cookiepath = '/', attackevasive = '0', disallowfloat = 'login|newthread|reply', creditnotice = '2|应届生币|枚', gid = 0, fid = parseInt('0'), tid = parseInt('0')</script>
+<script src="forumdata/cache/common.js?MxH" type="text/javascript"></script>
+</head>
+
+<body id="misc" onkeydown="if(event.keyCode==27) return false;">
+
+<div id="append_parent"></div><div id="ajaxwaitid"></div>
+
+<div id="header">
+<div class="wrap s_clear">
+<h2><a href="http://bbs.yingjiesheng.com/" title="应届生求职招聘论坛"><img border="0" alt="应届生BBS" src="/images/company/logo.gif"/></a></h2>
+<div id="umenu">
+<cite><a href="space-uid-896987.html" class="noborder">henrypope</a></cite><a href="my.php?item=threads&amp;srchfid=">我的帖子</a><span class="pipe">|</span>
+<a href="http://k.yingjiesheng.com/space.php?uid=896987" target="_blank">空间</a>
+<a id="myprompt" href="notice.php" class="new" onmouseover="showMenu({'ctrlid':this.id})">提醒</a>
+<span id="myprompt_check"></span>
+<a href="pm.php" id="pm_ntc" target="_blank">短消息</a>
+<a id="task_ntc" href="task.php" target="_blank">论坛任务</a>
+
+<span class="pipe">|</span>
+<a href="memcp.php">个人中心</a>
+<a href="logging.php?action=logout&amp;formhash=4464c515">退出</a>
+</div>
+<div id="umenu1">
+<a target="_blank" href="http://www.yingjiesheng.com/">应届生求职网首页</a><span class="pipe">|</span>
+<a target="_blank" href="http://www.yingjiesheng.com/commend-fulltime-1.html">全职推荐</a><span class="pipe">|</span>
+<a target="_blank" href="http://www.yingjiesheng.com/commend-parttime-1.html">实习推荐</a><span class="pipe">|</span>
+<a target="_blank" href="http://my.yingjiesheng.com/xuanjianghui.html">宣讲会</a><span class="pipe">|</span>
+<a target="_blank" href="http://zph.yingjiesheng.com/">招聘会</a><span class="pipe">|</span>
+<a target="_blank" href="http://www.yingjiesheng.com/industry/">行业招聘</a><span class="pipe">|</span>
+<a target="_blank" href="http://s.yingjiesheng.com/result.jsp?keyword=%E5%85%AC%E5%8A%A1%E5%91%98+OR+%E4%BA%8B%E4%B8%9A%E5%8D%95%E4%BD%8D&amp;do=2">公务员事业单位</a><span class="pipe">|</span>
+<a target="_blank" href="http://www.yingjiesheng.com/major/">分类求职</a><span class="pipe">|</span>
+<a target="_blank" href="http://www.yingjiesheng.com/deadline/">网申截止</a><span class="pipe">|</span>
+<a target="_blank" href="http://hotel.yingjiesheng.com/">求职旅社</a><span class="pipe">|</span>
+<a target="_blank" href="http://v.yingjiesheng.com/">视频</a><span class="pipe">|</span>
+<a target="_blank" href="http://my.yingjiesheng.com/index.php/personal/smsinfo.htm">短信</a><span class="pipe">|</span>
+<a target="_blank" href="http://k.yingjiesheng.com/center/">更多</a>
+</div>
+<div id="ad_headerbanner"><script type="text/javascript"><!--
+google_ad_client = "pub-5664019590241836";
+/* 728x90, 创建于 09-1-5,bbs_728_image */
+google_ad_slot = "8465360153";
+google_ad_width = 728;
+google_ad_height = 90;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script></div>
+
+</div>
+<div id="menu" class="s_clear">
+<div class="menul"></div>
+<ul class="s_clear">
+<li class="menu_27"><a href="http://k.yingjiesheng.com/center/" hidefocus="true" target="_blank" id="mn_center" style="font-weight: bold;color: red"><strong>我的应届生</strong></a></li><li class="menu_1"><a href="http://bbs.yingjiesheng.com" hidefocus="true" id="mn_bbs"><strong>论坛</strong></a></li><li class="menu_77"><a href="/forum-57-1.html" hidefocus="true" id="mn_forum-57-1"><strong>综合</strong></a></li><li class="menu_71"><a href="/forum-59-1.html" hidefocus="true" id="mn_forum-59-1"><strong>职业规划</strong></a></li><li class="menu_62"><a href="/forum-58-1.html" hidefocus="true" id="mn_forum-58-1"><strong>简历</strong></a></li><li class="menu_72"><a href="/forum-62-1.html" hidefocus="true" id="mn_forum-62-1"><strong>网申</strong></a></li><li class="menu_63"><a href="/forum-60-1.html" hidefocus="true" id="mn_forum-60-1"><strong>笔试</strong></a></li><li class="menu_64"><a href="/forum-61-1.html" hidefocus="true" id="mn_forum-61-1"><strong>面试</strong></a></li><li class="menu_73"><a href="/forum-433-1.html" hidefocus="true" id="mn_forum-433-1"><strong>签约</strong></a></li><li class="menu_75"><a href="/forum-427-1.html" hidefocus="true" id="mn_forum-427-1"><strong>薪资</strong></a></li><li class="menu_74"><a href="/forum-64-1.html" hidefocus="true" id="mn_forum-64-1"><strong>户口</strong></a></li><li class="menu_78"><a href="/index.php?gid=1151" hidefocus="true" id="mn_index_1"><strong>公务员</strong></a></li><li class="menu_76"><a href="/forum-436-1.html" hidefocus="true" id="mn_forum-436-1" style="color: red"><strong>大礼包</strong></a></li><li class="menu_79" id="cZ1ze8" onmouseover="showMenu({'ctrlid':this.id})"><a href="http://k.yingjiesheng.com/center/#sourceNav" hidefocus="true"  class="dropmenu"><strong>工具</strong></a></li><li class="menu_56"><a href="/thread-93508-1-1.html" hidefocus="true" target="_blank" id="mn_thread-93508-1-1" style="font-weight: bold;color: red"><strong>下载规则</strong></a></li></ul>
+<div class="menur"></div>
+</div>
+<div id="myprompt_menu" style="display:none" class="promptmenu">
+<div class="promptcontent">
+<ul class="s_clear"><li style="display:none"><a id="prompt_pm" href="pm.php?filter=newpm" target="_blank">私人消息 (0)</a></li><li style="display:none"><a id="prompt_announcepm" href="pm.php?filter=announcepm" target="_blank">公共消息 (0)</a></li><li style="display:none"><a id="prompt_task" href="task.php?item=doing" target="_blank">论坛任务 (0)</a></li><li><a id="prompt_systempm" href="notice.php?filter=systempm" target="_blank">系统消息 (1)</a></li><li style="display:none"><a id="prompt_friend" href="notice.php?filter=friend" target="_blank">好友消息 (0)</a></li><li style="display:none"><a id="prompt_threads" href="notice.php?filter=threads" target="_blank">帖子消息 (0)</a></li></ul>
+</div>
+</div>
+</div>
+<div id="nav"><a href="index.php">应届生求职招聘论坛</a> &raquo; 提示信息</div>
+
+<div id="wrap" class="wrap s_clear"><div class="main"><div class="content nofloat">
+<div class="fcontent alert_win">
+<h3 class="float_ctrl"><em>应届生求职招聘论坛 提示信息</em></h3>
+<hr class="shadowline" />
+<div class="postbox"><div class="alert_info">
+<p>对不起，您的应届生币不足(小于 0 枚)。<br /><br />[ <a href="/thread-93508-1-1.html" target=_blank>点击这里查看快速免费获取应届生币的方法</a> ]<br /><br />[ <a href="/thread-93248-1-1.html" target=_blank><font color="red">点击这里查看快速充值购买应届生币的方法</font></a> ]</p>
+</div></div>
+</div>
+</div></div></div>
+</div><ul class="popupmenu_popup headermenu_popup" id="plugin_menu" style="display: none">          <li><a id="mn_plink_wishwall" href="http://web.yingjiesheng.com/wishwall/">许愿墙</a></li>
+               <li><a id="mn_plink_offer" href="http://web.yingjiesheng.com/51offer/">Offer发送机</a></li>
+               <li><a id="mn_plink_flashgame" href="/plugins.php?p=nkflash"><font color=red>Flash小游戏</font></a></li>
+               <li><a id="mn_plink_sheep" href="/sheep.php"><font color=blue>扎绵羊小游戏</font></a></li>
+     </ul>
+<ul class="popupmenu_popup headermenu_popup" id="cZ1ze8_menu" style="display: none"><li><a href="http://my.yingjiesheng.com/index.php/personal/smsinfo.htm" hidefocus="true" target="_blank" >短信订阅名企招聘</a></li><li><a href="http://web.yingjiesheng.com/gpa/" hidefocus="true" target="_blank" >GPA在线计算器</a></li><li><a href="http://k.yingjiesheng.com/applymemo/tax.php" hidefocus="true" target="_blank" >工资计算器</a></li><li><a href="http://web.yingjiesheng.com/offer/" hidefocus="true" target="_blank" >Offer比较分析器</a></li><li><a href="http://web.yingjiesheng.com/wishwall/" hidefocus="true" target="_blank" >求职许愿墙</a></li><li><a href="http://web.yingjiesheng.com/51offer/" hidefocus="true" target="_blank" >Offer发送机</a></li><li><a href="http://vip.yingjiesheng.com/wap_yjs/" hidefocus="true" target="_blank" >手机上应届生</a></li><li><a href="http://web.yingjiesheng.com/jobtest/" hidefocus="true" target="_blank" >金庸趣味职业测试</a></li><li><a href="http://k.yingjiesheng.com/center/#sourceNav" hidefocus="true" >更多求职小工具</a></li></ul><div class="ad_footerbanner" id="ad_footerbanner1"><div style="width: 95%; margin: 0 auto; padding: 10px 5px; background: #fff;"><iframe scrolling="no" height="60" frameborder="no" width="750" class="search" marginheight="0" marginwidth="0" border="0" src="./iframe/search_thread_v1.html"></iframe>
+<script src="http://bbs.yingjiesheng.com/tag_search/ggword.js"></script></div></div><div class="ad_footerbanner" id="ad_footerbanner2"><script type="text/javascript"> /*960*90，创建于2010-8-5,bbs底部960 */ var cpro_id = 'u137213';</script>
+<script type="text/javascript" src="http://cpro.baidu.com/cpro/ui/c.js"></script></div>
+
+<div id="footer">
+<div class="wrap s_clear footerwidth">
+<div id="footlink">
+<p>
+<strong><a href="http://www.yingjiesheng.com/" target="_blank">应届生求职网YingJieSheng.COM</a></strong>
+( <a href="http://www.miibeian.gov.cn/" target="_blank">增值电信业务经营许可证沪B2-20070147</a>)<span class="pipe">|</span><a href="http://www.yingjiesheng.com/about/about.html" target="_blank">联系我们</a>
+<span class="pipe">|</span><a href="archiver/" target="_blank">Archiver</a><span class="pipe">|</span><a href="wap/" target="_blank">WAP</a></p>
+<p class="smalltext">
+GMT+8, 2010-9-7 20:29.
+</p>
+</div>
+<div id="rightinfo">
+<p>Powered by <strong><a href="http://www.discuz.net" target="_blank">Discuz!</a></strong> <em>7.2</em></p>
+<p class="smalltext">&copy; 2001-2009 <a href="http://www.comsenz.com" target="_blank">Comsenz Inc.</a></p>
+<div style="display:none;"><script src="http://count15.51yes.com/click.aspx?id=156561790&logo=5" type="text/javascript" type="text/javascript"></script><script src="http://count7.51yes.com/click.aspx?id=75689736&logo=5" type="text/javascript" type="text/javascript"></script>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-2434975-7");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</div>
+</div></div>
+</div>
+</body>
+</html>
